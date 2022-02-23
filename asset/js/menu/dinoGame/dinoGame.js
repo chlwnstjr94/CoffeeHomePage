95853d921447fbdfbaa7d7a968357e9838298572
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-02-23 22:39:20 +0900

    modify dinoGame_main - add obstacle move

--- asset/js/menu/dinoGame/dinoGame.js
+++ asset/js/menu/dinoGame/dinoGame.js
@@ -4,6 +4,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let g_game_status = false;
+let g_obstacle_interval = null;
+
+function showCoords(event) {
+    let x = event.clientX;
+    let y = event.clientY;
+    console.log("x: "+x+" y: "+y);
+}
+
 function init() {
     // player
     let player = new Player();
@@ -11,13 +20,23 @@ function init() {
     player.draw();
     
     // 장애물
-    let obstacle = new Obstacle();
+    let high_obstacle = new Obstacle(true);
 
-    obstacle.slide();
+    let low_obstacle = new Obstacle(false);
+
+    g_obstacle_interval = setInterval(function() {
+        high_obstacle.run(high_obstacle);
+        low_obstacle.run(low_obstacle);
+    }, 1)
+
+    if(g_game_status) {
+        clearInterval(g_obstacle_interval);
+    }
 
     // click 이벤트
-    canvas.addEventListener('click', function() {
+    canvas.addEventListener('click', function(event) {
         player.jump();
+        showCoords(event);
     });
 };
 
